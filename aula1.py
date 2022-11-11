23aa8c241c3e0be141b44bfe130abe6c41309d0d
João Sales <joaovictorlsales@gmail.com>
2022-11-11 19:24:26 -0300

    Comentarios e DocStrings

---
+++ aula1.py
@@ -0,0 +1,15 @@
+"""
+Isso é uma DocString
+OBS.: O python não ignora as doctrings, diferente dos comentários
+com #, ele lê essas linhas.
+Oficialmente isso não é um comentário mas é comumente
+utilizado como comentários de múltiplas linhas
+pelos desenvolvedores.
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
