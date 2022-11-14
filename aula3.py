7837a880304b439c99e0d1602828c195901a4e43
João Sales <joaovictorlsales@gmail.com>
2022-11-14 14:08:16 -0300

    Strings

---
+++ aula3.py
@@ -0,0 +1,23 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Joao Sales')
+print(1, 'Joao "Sales"')
+
+# Aspas duplas
+print("Joao Sales")
+print(2, "Joao 'Sales'")
+
+# Escape
+print("Joao \"Sales\"")
+
+# r
+print(r"Joao \"Sales\"")
